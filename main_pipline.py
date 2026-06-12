4fb2abd892f0167cf4ec1ce6539c8f9c5c398ba8
Alsani Abdul Rofiq <xsanz022@gmail.com>
2026-06-12 22:38:47 +0700

    add basic image preprocessing module

--- main_pipline.py
+++ main_pipline.py
@@ -2,3 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def preprocess_image(image_path):
+    """Membaca citra, konversi ke grayscale, dan normalisasi dimensi (512x512)."""
+    img = cv2.imread(image_path, 0)
+    if img is None:
+        raise ValueError(f"Galat: Citra '{image_path}' tidak ditemukan di direktori.")
+    
+    img_resized = cv2.resize(img, (512, 512))
+    return img_resized
