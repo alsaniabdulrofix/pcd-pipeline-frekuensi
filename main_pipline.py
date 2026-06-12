c5eddc12bc7e3696cec2f25d87e2f3d70de0aa06
Alsani Abdul Rofiq <xsanz022@gmail.com>
2026-06-12 22:51:00 +0700

    delete unsuse

--- main_pipline.py
+++ main_pipline.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # TAHAP 1: MODUL PREPROCESSING
 # ==========================================
 def preprocess_image(image_path):
-    """Membaca citra, konversi ke grayscale, dan normalisasi dimensi (512x512)."""
+    
     img = cv2.imread(image_path, 0)
     if img is None:
         raise ValueError(f"Galat: Citra '{image_path}' tidak ditemukan di direktori.")
@@ -19,14 +19,14 @@ def preprocess_image(image_path):
 # TAHAP 2: MODUL TRANSFORMASI SPEKTRUM (FFT)
 # ==========================================
 def transform_to_frequency(img):
-    """Konversi ranah spasial ke ranah frekuensi beserta pergeseran ke pusat matriks."""
+   
     f_transform = np.fft.fft2(img)
     f_shift = np.fft.fftshift(f_transform)
     return f_shift
 
 
 def create_lpf_mask(shape, radius=60):
-    """Konstruksi Ideal Low Pass Filter (mempertahankan frekuensi rendah/warna dasar)."""
+    
     baris, kolom = shape
     pusat_baris, pusat_kolom = baris // 2, kolom // 2
     
@@ -36,7 +36,7 @@ def create_lpf_mask(shape, radius=60):
     return mask
 
 def create_hpf_mask(shape, radius=60):
-    """Konstruksi Ideal High Pass Filter (mempertahankan frekuensi tinggi/tepi objek)."""
+    
     baris, kolom = shape
     pusat_baris, pusat_kolom = baris // 2, kolom // 2
     
